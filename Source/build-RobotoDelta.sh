e445729ba0b69580a17cb73b65d786fe000403fa
Santiago Orozco <santiagorozco@gmail.com>
2025-10-17 09:39:08 -0600

    Update build-RobotoDelta.sh

--- Source/build-RobotoDelta.sh
+++ Source/build-RobotoDelta.sh
@@ -3,12 +3,8 @@ source tools/roboto-delta-env/bin/activate
 
 ## MAKE VF
 
-## full glyphset
-fontmake -m source/Parametric-avar2/Roboto-Delta.designspace -o variable --output-path fonts/LGCAlpha/Roboto-Delta-VF.ttf --no-production-names
-fontmake -m source/Parametric-avar2/Roboto-Delta-no-fences.designspace -o variable --output-path fonts/LGCAlpha/Roboto-Delta-no-fences-VF.ttf --no-production-names
-fontmake -m source/Parametric-avar2/Roboto-Delta-no-slant.designspace -o variable --output-path fonts/LGCAlpha/Roboto-Delta-no-slant-VF.ttf --no-production-names
+fontmake -m source/Roman/RobotoDelta-Roman.designspace -o variable --output-path fonts/LGCAlpha/RobotoDelta-Roman-VF.ttf --no-production-names --no-check-compatibility
 
-## MAKE INSTANCES
-#fontmake -m source/Parametric-avar2/RobotoAvar2-avar2.designspace -i -o ufo --output-dir source/Parametric-avar2/instances/
+fontmake -m source/Italic/RobotoDelta-Italic.designspace -o variable --output-path fonts/LGCAlpha/RobotoDelta-Italic-VF.ttf --no-production-names --no-check-compatibility
 
 deactivate
